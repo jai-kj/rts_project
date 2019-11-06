5b56020f499835236f018b1d812950518605fa3c
jaijain230999 <35729100+jaijain230999@users.noreply.github.com>
2019-11-07 00:12:58 +0530

    Create admin.php

---
+++ admin/admin.php
@@ -0,0 +1,12 @@
+<?php include('admin_urlCheck.php');
+include("../user/header.php"); ?>
+<link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <?php include('admin_nav.php'); ?>
+    <?php include('insertTrain.php'); ?>
+    <?php include('updateTrain.php'); ?>
+</body>
+<script type="text/javascript" src="../js/admin.js"></script>
+
+<?php include("../user/footer.php"); ?>
